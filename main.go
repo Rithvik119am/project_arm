d449f9edd92bbca08c32e8ddc1ad93955ae4bcb2
Rithvik119am <rithvikreddy524@gmail.com>
2023-11-08 14:54:27 +0530

    button file changes

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"text/template"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	/*postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	"password=%s dbname=%s sslmode=disable",
+	host, port, user, password, dbname)*/
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmp := template.Must(template.ParseFiles("templates/index.html"))
+		tmp.Execute(w, nil)
+	})
+	http.HandleFunc("/getdata", func(w http.ResponseWriter, r *http.Request) {
+		tmp1 := template.Must(template.ParseFiles("templates/other.html"))
+		tmp1.Execute(w, nil)
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
