04811e48ef49df1a325b8494fecc35707c5c2ff3
Rithvik119am <rithvikreddy524@gmail.com>
2023-11-08 14:50:48 +0530

    button page changing

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	//"fmt"
-	"fmt"
+
+	"html/template"
 	"net/http"
 )
 
@@ -25,12 +26,18 @@ const (
 )
 
 func main() {
-	postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	/*postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	"password=%s dbname=%s sslmode=disable",
+	host, port, user, password, dbname)*/
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, postgresqlDbInfo)
+		tmp := template.Must(template.ParseFiles("templates/index.html"))
+		tmp.Execute(w, nil)
+	})
+	http.HandleFunc("/getdata", func(w http.ResponseWriter, r *http.Request) {
+		tmp1 := template.Must(template.ParseFiles("templates/other.html"))
+		tmp1.Execute(w, nil)
 	})
 
+	http.ListenAndServe(":8000", nil)
 }
