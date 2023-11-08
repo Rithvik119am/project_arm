0a9bc9cf1c2bb40dc9a9008900e36a480567e8e3
Aman Davuluru <amandavu11@gmail.com>
2023-11-08 06:39:30 +0000

    This is the First commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	//"fmt"
+	"fmt"
+	"net/http"
+	"database"
+)
+
+type Team struct {
+	name            string
+	matches_played  int
+	won             int
+	lost            int
+	draw            int
+	goal_difference int
+	points          int
+}
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "gitpod"
+	password = "root"
+	dbname   = "teams"
+)
+
+func main() {
+	postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, postgresqlDbInfo)
+	})
+
+}
